e3050c84e415257e76606ee1ee588bf945c83ebc
jony111M <pennyc2Stealer@proton.me>
2026-07-11 14:51:50 +0100

    add the zod

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -1,37 +1,55 @@
 import * as z from "zod";
 
 // validation the post request  and put body data
+
+const validationAffectationZod = z.object({
+    arbitreId: z.number().int({
+        message: "arbitchId type is number"
+    }),
+    matchId: z.number().int({
+        message: "matchId type is number"
+    }),
+    role: z.enum(["central", "assistant", "var", "avar", "4e"])
+})
+
 export const validationdAffectation = async(req, res, next) => {
-    const {arbitreId, matchId, role} = req.body;
-    if(!arbitreId || !matchId || !role) return res.status(400).json({message: "error in request"});
-    const enumDataCheck = ["central", "assistant", "var", "avar", "4e"]
-    if(!enumDataCheck.includes(role)) return res.status(400).json({message: "error in request"});
+    const result = validationAffectationZod.safeParse(req.body);
+    if(!result) return res.status(400).json({message: "error in request"});
     next()
 }
  
-// validation the  post request and put body data from match
+// create object zod validation match 
+const validateMathZod = z.object({
+    equipeDomicile: z.string().min(1, "error 1"),
+    equipeExterieur: z.string().min(1, "error 2"),
+    stade: z.string().min(1, "error 3"),
+    villeHote: z.string().min(1, "error 4"),
+    dateMatch: z.string().datetime(),
+    phase: z.enum(["Groupes", "8e", "4e", "demi", "finale"])
+    
+})
 export const validationMatch = async(req, res, next) => {
-    const {equipeDomicile, equipeExterieur, stade, villeHote, dateMatch, phase} = req.body;
-    if(!equipeDomicile || !equipeExterieur || !stade || !villeHote || !dateMatch || !phase) return res.status(400).json({message: "error in request"});
-    const matchPhase = ["Groupes", "8e", "4e", "demi", "finale"]
-    if(!matchPhase.includes(phase)) return res.status(400).json({message: "error in request"});
+    const validtionResult = validateMathZod.safeParse(req.body);
+    if(!validtionResult.success) return res.status(400).json({message: "error in request"});
     next()
 }
 
+// create object zod validation arbite
+const validationArbritZod = z.object({
+    nom: z.string().min(2),
+    prenom: z.string().min(2),
+    nationalite: z.string().min(2),
+    confedeation: z.enum(["uefa", "caf", "afc", "conmebol", "concacaf", "ofc"]),
+    categorie: z.enum(["central", "assistant", "var", "avar", "fourth official"]),
+    experience: z.number().int({
+        message : "Type error experience"
+    }),
+    statut: z.enum(["actif", "suspendu", "blesse", "retraite"])
+});
+
 export const validationarbite = async(req, res, next) => {
-    const {nom, prenom, nationalite, confedeation, categorie, experience, statut} = req.body;
-    if(!nom || !prenom || !nationalite || !confedeation || !categorie || !experience || !statut) return res.status(400).json({message: "error in request"});
-    const dataValidation = {
-        confedeation : ["uefa", "caf", "afc", "conmebol", "concacaf", "ofc"],
-        categorie : ["central", "assistant", "var", "avar", "fourth official"],
-        statut : ["actif", "suspendu", "blesse", "retraite"]
-    }
-//next
-    if(
-        !dataValidation.confedeation.includes(confedeation) || 
-        !dataValidation.categorie.includes(categorie) ||
-        !dataValidation.statut.includes(statut)
-    ) return res.status(400).json({message : "error in request"})
+    const result = validationArbritZod.safeParse(req.body);
+    if(!result.success) return res.status(400).json({message: "error in request"});
     next()
 }
 
@@ -51,13 +69,18 @@ export const validationLogin = async(req, res, next) => {
         return res.statut(400).json({message : "error in login "})
     }
     next()
-}
+};
 
 // i add zod in validationRegerster function
+
+const validationRegesterZod = z.object({
+    username : z.string().min(4, "error in username size"),
+    email : z.string().min(1, "error in email size").email(),
+    password: z.min(8, "error the password size 8"),
+    role: z.enum(["admin", "commissaire", "arbitre", "consultation"])
+});
 export const validationRegerster = async(req, res, next) => {
-    const {username, email, password, role} = req.body;
-    if(!username || !email || !password) return res.status(400).json({message : "5ona da5le"})
-    const roles = ["admin", "commissaire", "arbitre", "consultation"];
-    if(!roles.includes(role)) return res.status(400).json({message : "Role is not valid"});
+    const result = validationRegesterZod.safeParse(req.body);
+    if(!result.success) return res.status(400).json({message : "Role is not valid"});
     next()
-}
+};
